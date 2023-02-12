5f460e388dad7c40e36780a1138e8c2756e325c9
AhmedNabilSharawy <ahmednabilsharawy@gmail.com>
2023-02-12 18:16:28 +0200

    fixing not existance of database directry in list script

--- scripts/db/list.sh
+++ scripts/db/list.sh
@@ -12,29 +12,26 @@ shopt -s extglob
 export LC_COLLATE=C
 
 listDB(){
-    local dbs=($(ls ./Databases))
-    local dbsLength=${#dbs[*]}
-    local msg="\nDatabases is empty\n"
-    [ $dbsLength -eq 0 ] && echo -e $msg && return
-    customMenu "List of Databases     " 1 ${dbs[*]}
+    if [[ -d ./Databases ]];then       # -n $DbName checks that input is not empty 
+        if [[ "$(ls ./Databases)" ]]; then
+        
+            clear #formating
+
+            ls ./Databases
+            echo #formating new line
+
+            #Display menu to user
+            askMenuWithoutCont
+        else
+            clear #formating 
+            echo "Database is empty"
+            echo #formating new line
+            #Display menu to user
+            askMenuWithoutCont
+        fi
+    else
+        echo "No Databases created yet!"
+        echo #formating new line
+        askMenuWithoutCont
+    fi
 }
-#listDB
-
-#     # -n $DbName checks that input is not empty 
-#     if [[ "$(ls ./Databases)" ]]; then
-    
-#         clear #formating
-
-#         ls ./Databases
-#         echo #formating new line
-
-#         #Display menu to user
-#         askMenuWithoutCont
-#     else
-#         clear #formating 
-#         echo "Database is empty"
-#         echo #formating new line
-#         #Display menu to user
-#         askMenuWithoutCont
-#     fi
-# done
